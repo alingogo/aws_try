866a59669c60cecf61e185dc0fd6dc4263c6a399
EC2 Default User <ec2-user@ip-10-158-170-28.ap-northeast-1.compute.internal>
2012-07-26 04:24:35 +0000

    a

--- sqs/send_message.rb
+++ sqs/send_message.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 # -*- encoding: utf-8 -*-
-# Copyright 2007 Amazon Technologies, Inc.  Licensed under the Apache License, Version 2.0 (the "License"); 
+# Copyright 2007 Amazon Technologies, Inc.  Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License. You may obtain a copy of the License at:
 #
 # http://aws.amazon.com/apache2.0
 #
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
 require "rubygems"
@@ -13,6 +13,7 @@ require "aws-sdk"
 require File.join(File.dirname(__FILE__), "../samples_config")
 require "upload_file"
 require "json"
+require "yaml"
 
 AMAZON_SQS_TEST_QUEUE = "SQS-Test-Queue-Ruby"
 
@@ -20,7 +21,7 @@ sqs = AWS::SQS.new
 
 queue = sqs.queues.named(AMAZON_SQS_TEST_QUEUE)
 
-t = ARGV[0].to_i || 10
+t = (ARGV[0] || 10).to_i
 t.times do |i|
   bucket_name = "8342_bucket_" + rand(100000).to_s
   file_name = "sample_original.txt"
